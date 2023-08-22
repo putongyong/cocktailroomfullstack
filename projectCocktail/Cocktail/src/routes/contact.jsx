e9d4fc7fa04fcbb886c904ac321986f782b4f29d
putongyong <117098471+putongyong@users.noreply.github.com>
2023-08-23 00:07:34 +0800

    modify google location

--- projectCocktail/Cocktail/src/routes/contact.jsx
+++ projectCocktail/Cocktail/src/routes/contact.jsx
@@ -62,7 +62,7 @@ export default function Contact() {
 				<div class="row">
 					<div class="col-12">
           				<div class="tm-map">
-							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11196.961132529668!2d-43.38581128725845!3d-23.011063013218724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9bdb695cd967b7%3A0x171cdd035a6a9d84!2sAv.%20L%C3%BAcio%20Costa%20-%20Barra%20da%20Tijuca%2C%20Rio%20de%20Janeiro%20-%20RJ%2C%20Brazil!5e0!3m2!1sen!2sth!4v1568649412152!5m2!1sen!2sth" frameborder="0" style={{border: 0}} allowfullscreen=""></iframe>
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d83998.96798474021!2d2.2644627277013405!3d48.85882536954717!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2z5be06buO!5e0!3m2!1szh-CN!2sfr!4v1692719168342!5m2!1szh-CN!2sfr" frameborder="0" style={{border: 0}} allowfullscreen=""></iframe>
 						</div>
 					</div>
 				</div>
