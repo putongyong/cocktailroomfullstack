f04514ef7d217d994e789fc5856e3da1ad9175e6
putongyong <117098471+putongyong@users.noreply.github.com>
2023-08-22 17:58:14 +0800

    update style

--- projectCocktail/Cocktail/src/routes/components/info.jsx
+++ projectCocktail/Cocktail/src/routes/components/info.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom"
+
 export default function Info(){
     return (
         <>	
@@ -5,14 +7,14 @@ export default function Info(){
 				<div class="row">
 					<div class="col-md-6">
 						<figure class="tm-description-figure">
-							<img src="/img/pexels-chan-walrus-941864.jpg" alt="Image" class="img-fluid" />
+							<img src="/product_images/pexels-chan-walrus-941864_compressed.jpg" alt="Image" class="img-fluid" />
 						</figure>
 					</div>
 					<div class="col-md-6">
 						<div class="tm-description-box"> 
 							<h4 class="tm-gallery-title">Maecenas nulla neque</h4>
 							<p class="tm-mb-45">Redistributing this template as a downloadable ZIP file on any template collection site is strictly prohibited. You will need to <a rel="nofollow" href="https://templatemo.com/contact">talk to us</a> for additional permissions about our templates. Thank you.</p>
-							<a href="about.html" class="tm-btn tm-btn-default tm-right">Read More</a>
+							<NavLink to="/apropos" class="tm-btn tm-btn-default tm-right">Read More</NavLink>
 						</div>
 					</div>
 				</div>
--- projectCocktail/Cocktail/src/routes/components/intro.jsx
+++ projectCocktail/Cocktail/src/routes/components/intro.jsx
@@ -3,7 +3,7 @@ export default function Intro(){
         <>
             <div className="video-background">
 				<video autoPlay loop muted>
-					<source src="../video/pexels-cottonbro-studio-3188951-1920x1080-25fps.mp4" type="video/mp4" />
+					<source src="/product_images/16f00026-c8e0-4ef9-a3c8-8fd54acdb047.mp4" type="video/mp4" />
 				</video>
 				<div className="text-container">
 					<h1>Welcome to my website!</h1>
