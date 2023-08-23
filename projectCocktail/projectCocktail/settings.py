010de20760eabdc2b607cd45b816c895b6a9dce2
putongyong <117098471+putongyong@users.noreply.github.com>
2023-08-24 03:55:19 +0800

    update

--- projectCocktail/projectCocktail/settings.py
+++ projectCocktail/projectCocktail/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-
 from pathlib import Path
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'projectCocktail.urls'
@@ -132,3 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/product_images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'product_images')
 
+# Configure Django App for Heroku.
+
+django_on_heroku.settings(locals())
