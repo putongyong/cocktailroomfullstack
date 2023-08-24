13f10160de69c784d0f41f6fec6c899d3824b5d7
putongyong <117098471+putongyong@users.noreply.github.com>
2023-08-24 17:20:02 +0800

    modified

--- projectCocktail/projectCocktail/settings.py
+++ projectCocktail/projectCocktail/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,6 +132,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/product_images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'product_images')
 
-# Configure Django App for Heroku.
-
-django_on_heroku.settings(locals())
